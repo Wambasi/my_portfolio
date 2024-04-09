d03c0c05173ded78f95379d23bb4aea761683dfd
Wambasi <60027629+Wambasi@users.noreply.github.com>
2024-04-09 18:50:09 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('#nav-toggle');
+    const navbar = document.querySelector('#navbar');
+  
+    navToggle.addEventListener('click', () => {
+      navbar.classList.toggle('hidden');
+    });
+  });
